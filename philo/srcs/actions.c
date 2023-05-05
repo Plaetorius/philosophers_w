eb4d1ca344717488f1d8d4f73f1dbb68b8e25edb
Tag6362 <tag6362@gmail.com>
2023-05-05 17:40:00 +0200

    Fixed on pc

--- philo/srcs/actions.c
+++ philo/srcs/actions.c
@@ -6,7 +6,7 @@
 /*   By: tgernez <tgernez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 16:10:24 by tgernez           #+#    #+#             */
-/*   Updated: 2023/05/05 13:52:54 by tgernez          ###   ########.fr       */
+/*   Updated: 2023/05/05 17:33:14 by tgernez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,14 @@ void	eat(t_philo *philo, t_vars *vars)
 		pthread_mutex_unlock(&vars->m_ate_enough);
 	}
 	ft_usleep(vars->tte);
-	pthread_mutex_unlock(&philo->fork);
+	if (philo->nb == 2)
+	{
+		pthread_mutex_unlock(&philo->fork);
+		pthread_mutex_unlock(&philo->next->fork);
+		return ;
+	}
 	pthread_mutex_unlock(&philo->next->fork);
+	pthread_mutex_unlock(&philo->fork);
 }
 
 /*
@@ -35,7 +41,7 @@ void	eat(t_philo *philo, t_vars *vars)
 */
 void	take_forks(t_philo *philo, t_vars *vars)
 {
-	if (philo->nb == 1)
+	if (philo->nb == 2)
 	{
 		pthread_mutex_lock(&philo->fork);
 		print_action(philo, TAKEN_FORK, vars);
